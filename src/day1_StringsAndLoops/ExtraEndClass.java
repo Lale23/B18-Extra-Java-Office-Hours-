f36943ca060ba22c86de6027268f386440013ea5
saimCybertek <saim@cybertekschool.com>
2020-05-02 15:47:48 -0500

    Day 1 - Strings and Loops

--- src/day1_StringsAndLoops/ExtraEndClass.java
+++ src/day1_StringsAndLoops/ExtraEndClass.java
@@ -1,4 +1,21 @@
 package day1_StringsAndLoops;
 
 public class ExtraEndClass {
+
+    public static void main(String []args) {
+        String s = extraEnd("hello");
+        System.out.println(s);
+    }
+
+
+    public static String extraEnd(String str) {
+
+        String end = str.substring(str.length()-2);
+
+        return end + end + end;
+
+    }
+
+
+
 }
--- src/day1_StringsAndLoops/TheEndClass.java
+++ src/day1_StringsAndLoops/TheEndClass.java
@@ -1,4 +1,21 @@
 package day1_StringsAndLoops;
 
 public class TheEndClass {
+    public static void main(String[] args) {
+        System.out.println(firstTwo("hello"));
+        System.out.println(firstTwo(""));
+        System.out.println(firstTwo("g"));
+    }
+
+    public static String firstTwo(String str) {
+
+        if(str.length() < 2) return str;
+
+        return str.substring(0,2);
+
+        // return str.length() >= 2 ? str.substring(0,2)  : str;
+
+    }
+
+
 }
